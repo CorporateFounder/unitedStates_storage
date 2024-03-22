ab0cbf6af5a85059cd7de46545bb2a34d0e2d322
CorporateFounder <CorporateFounder@proton.me>
2024-03-22 20:19:08 +0500

    release 0.0.10 faster updating UtilsResolving.java helpResolve3

--- src/main/java/International_Trade_Union/setings/Seting.java
+++ src/main/java/International_Trade_Union/setings/Seting.java
@@ -190,7 +190,8 @@ public interface Seting {
 
 
     //адресса внешних сервисов
-    Set<String> ORIGINAL_ADDRESSES = Set.of("http://194.87.236.238:82");
+    Set<String> ORIGINAL_ADDRESSES = IS_TEST ? Set.of("http://localhost:82"): Set.of("http://194.87.236.238:82");
+
     //    Set<String> ORIGINAL_ADDRESSES = Set.of("http://localhost:8083");
     Set<String> ORIGINAL_BLOCKED_ADDRESS = Set.of("http://154.40.38.130:80",
             "http://10.0.36.2:80");
