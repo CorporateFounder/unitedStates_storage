9d42f158d55c40a57b2ca4e7b3664bea907dc0ee
CorporateFounder <CorporateFounder@proton.me>
2024-05-25 19:06:14 +0500

    182

--- src/main/java/International_Trade_Union/utils/UtilsTime.java
+++ src/main/java/International_Trade_Union/utils/UtilsTime.java
@@ -1,34 +1,80 @@
 package International_Trade_Union.utils;
 
 import International_Trade_Union.logger.MyLogger;
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class UtilsTime {
     public static long getUniversalTimestamp() {
         return ZonedDateTime.now(ZoneOffset.UTC).toEpochSecond() * 1000L;
     }
 
-
-
-    // Возвращает временную метку в текстовом формате UTC
-    String getUniversalTimestampString() {
+    public static String getUniversalTimestampString() {
         return ZonedDateTime.now(ZoneOffset.UTC).toString();
     }
 
     public static long differentMillSecondTime(long first, long second){
         return second - first;
     }
-    // Новый метод получения временной метки UTC через API
 
+    public static long getUniversalTimestampFromHttp() {
+        try {
+            URL url = new URL("http://worldtimeapi.org/api/timezone/Etc/UTC");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+            in.close();
+
+            String jsonResponse = response.toString();
+            long unixTime = Long.parseLong(jsonResponse.replaceAll(".*\"unixtime\":(\\d+).*", "$1"));
+            return unixTime * 1000;
+        } catch (IOException e) {
+            e.printStackTrace();
+            MyLogger.saveLog("UtilsTime HTTP error: ", e);
+            return Instant.now().getEpochSecond() * 1000;
+        }
+    }
+
+    public static long getUniversalTimestampFromNTP() {
+        String[] ntpServers = {
+                "pool.ntp.org"
+        };
+
+        for (String server : ntpServers) {
+            try {
+                NTPUDPClient client = new NTPUDPClient();
+                client.setDefaultTimeout(5000);
+                InetAddress address = InetAddress.getByName(server);
+                TimeInfo timeInfo = client.getTime(address);
+                long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime() * 1000;
+                client.close();
+                return returnTime;
+            } catch (Exception e) {
+                e.printStackTrace();
+                MyLogger.saveLog("UtilsTime NTP error with server " + server + ": ", e);
+            }
+        }
+
+        // If all NTP servers fail, fall back to HTTP
+        return getUniversalTimestampFromHttp();
+    }
 }
-
-
