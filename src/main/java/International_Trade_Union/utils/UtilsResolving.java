616ee12d08f5c4b1627fd0ab822e2e1a8db018d9
CorporateFounder <CorporateFounder@proton.me>
2024-07-11 17:14:13 +0500

    220 тестовая версия

--- src/main/java/International_Trade_Union/utils/UtilsResolving.java
+++ src/main/java/International_Trade_Union/utils/UtilsResolving.java
@@ -678,11 +678,8 @@ public class UtilsResolving {
     public boolean isBig(
             DataShortBlockchainInformation actual,
             DataShortBlockchainInformation global) {
-        if(global.getSize() == actual.getSize() && global.getBigRandomNumber() > actual.getBigRandomNumber()){
-            return true;
-        }
         if (global.getSize() >= actual.getSize() - Seting.IS_BIG_DIFFERENT
-                && global.getBigRandomNumber() > actual.getBigRandomNumber() + (prevBlock().getHashCompexity() * 55)) {
+                && global.getBigRandomNumber() > actual.getBigRandomNumber()) {
             return true;
         }
         return false;
