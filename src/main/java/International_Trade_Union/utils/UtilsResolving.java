f3143334e420779ef8010622463bbf088da4713c
CorporateFounder <CorporateFounder@proton.me>
2024-09-08 21:45:04 +0500

    323 test

--- src/main/java/International_Trade_Union/utils/UtilsResolving.java
+++ src/main/java/International_Trade_Union/utils/UtilsResolving.java
@@ -1416,11 +1416,9 @@ public class UtilsResolving {
 
                 EntityBlock entityBlock = UtilsBlockToEntityBlock.blockToEntityBlock(block);
                 list.add(entityBlock);
-
-
                 calculateBalance(balances, block, signs);
-
                 blockService.saveAllBLockF(list);
+                list = new ArrayList<>();
 
 
                 tempBalances = UtilsUse.differentAccount(tempBalances, balances);
@@ -1433,12 +1431,8 @@ public class UtilsResolving {
             }
 //        UtilsJson.saveWindowsToFile(windows, Seting.SLIDING_WINDOWS_BALANCE);
 
-            list = list.stream().sorted(Comparator.comparing(EntityBlock::getSpecialIndex)).collect(Collectors.toList());
+//        list = list.stream().sorted(Comparator.comparing(EntityBlock::getSpecialIndex)).collect(Collectors.toList());
             // Вызов getLaws один раз для всех блоков
-
-            long finish = UtilsTime.getUniversalTimestamp();
-            System.out.println("UtilsResolving: addBlock3: for: time different: " + UtilsTime.differentMillSecondTime(start, finish));
-
 
 
         } catch (Exception e) {
