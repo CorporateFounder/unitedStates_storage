991efe3a7e049770e77d5b707f4a071ae9069dfc
CorporateFounder <CorporateFounder@proton.me>
2024-12-29 18:22:51 +0500

    418 final

--- src/main/java/International_Trade_Union/controllers/ConductorController.java
+++ src/main/java/International_Trade_Union/controllers/ConductorController.java
@@ -570,7 +570,7 @@ public class ConductorController {
         }
     }
 
-    @GetMapping("/findBlocksBySenderInRange")
+    @GetMapping("/findBlocksByCustomerInRange")
     public List<EntityBlock> getBlocksByCustomer(
             @RequestParam long from,
             @RequestParam long to,
